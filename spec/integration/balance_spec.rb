80dfa6f162049fb9481ca298ba25e68e65d3121a
Constantin Gavrilescu <comisarulmoldovan@gmail.com>
2011-11-07 23:00:48 -0600

    Removed started but unfinished test.

--- spec/integration/balance_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-describe 'Balance calculation' do
-  it 'adds up balance from transactions' do
-     cc = CreditCard.new(:credit_limit => 2000, :person => "Tom")
-     
-  end
-end
-
